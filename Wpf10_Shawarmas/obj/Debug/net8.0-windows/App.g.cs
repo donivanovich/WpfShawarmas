1e75104e4cacb941216c0555a6c0550417ca6f84
javierJ-S <javier.jimenez.simon@alumnojoyfe.iepgroup.es>
2026-01-26 12:18:13 +0100

    prueba

--- Wpf10_Shawarmas/obj/Debug/net8.0-windows/App.g.cs
+++ Wpf10_Shawarmas/obj/Debug/net8.0-windows/App.g.cs
@@ -47,7 +47,7 @@ namespace Wpf10_Shawarmas {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.10.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace Wpf10_Shawarmas {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.10.0")]
         public static void Main() {
             Wpf10_Shawarmas.App app = new Wpf10_Shawarmas.App();
             app.InitializeComponent();
